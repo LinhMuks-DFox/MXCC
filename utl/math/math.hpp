b7c5107f6455a19fd62e702b4471fc890cb3bccc
dotFoxyyyy <muxxum65536@gmail.com>
2022-01-13 15:42:54 +0900

    quick sqrt algorithm i_sqrt added.

--- utl/math/math.hpp
+++ utl/math/math.hpp
@@ -13,7 +13,18 @@ namespace MXC::Math {
 
 #pragma region logarithm
 
+
     [[deprecated("Unimplemented yet.")]] static inline float64
+    log_2(float64 v) {}
+
+    [[deprecated("Unimplemented yet.")]] static inline float64
+    log_exp(float64 v) {}
+
+    [[deprecated("Unimplemented yet.")]] static inline float64 log_n() {}
+
+    [[deprecated("Unimplemented yet.")]] static inline float64 log10() {}
+    [[deprecated("Unimplemented yet.")]] static inline float64
+
     log_base(float64 value, float64 base) {
         /*!
          * log function in MXC return log value same as std::log()
@@ -26,28 +37,15 @@ namespace MXC::Math {
         if (float64_eq(value, 0.0)) return neg_infinity();
         if (value < 0.0) return nan();
         // TODO: implement log_base.(logarithm of x base b = log(x)/log(b))
+
+        return log_2(base) / log_2(value);
     }
-
-    [[deprecated("Unimplemented yet.")]] static inline float64
-    log_2(float64 v) {
-        if (float64_eq(v, E)) return LOG2_E;
-        return log_base(v, 2.);
-    }
-
-    [[deprecated("Unimplemented yet.")]] static inline float64
-    log_exp(float64 v) {
-        return log_base(v, E);
-    }
-
-    [[deprecated("Unimplemented yet.")]] static inline float64 log_n() {}
-
-    [[deprecated("Unimplemented yet.")]] static inline float64 log10() {}
-
     // From: https://www.zhihu.com/question/35122102
     template<typename T>
     struct i_sqrt_traits {
         static_assert(std::is_unsigned<T>::value,
                       "Generic i_sqrt only on unsigned types");
+
         static size_t bitCount(const T &n) {
             T a(n);
             size_t count = 0;
@@ -58,10 +56,11 @@ namespace MXC::Math {
             return count;
         }
 
-        static uint8_t extractTowBitsAt(const T &n, size_t i) {
+        static uint8_t extract_tow_bits_at(const T &n, size_t i) {
             return static_cast<uint8_t>((n >> i) & 3);
         }
     };
+
     template<typename T>
     T i_sqrt(const T &n) {
         T remainder{}, root{};
@@ -71,7 +70,7 @@ namespace MXC::Math {
             root <<= 1;
             ++root;
             remainder <<= 2;
-            remainder |= i_sqrt_traits<T>::extractTowBitsAt(n, i);
+            remainder |= i_sqrt_traits<T>::extract_tow_bits_at(n, i);
             if (root <= remainder) {
                 remainder -= root;
                 ++root;
@@ -81,14 +80,17 @@ namespace MXC::Math {
 
         return root >>= 1;
     }
+
     bool is_prime(const uint64 &num) {
         if (num < 1) return false;
+        if (num == 2) return true;
         auto limit = i_sqrt<uint64>(num);
         for (auto i = 2; i < limit; ++i) {
             if (num % i == 0) { return false; }
         }
         return true;
     }
+
 #pragma endregion
 }// namespace MXC::Math
 #endif//MXC_MATH_HPP
